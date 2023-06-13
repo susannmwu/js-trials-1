702f7cb5967e7b98698bd4a9a1d864b18240615b
Susan Wu <susannmwu@gmail.com>
2023-06-13 12:51:06 -0700

    Completed trials.py

--- trials.py
+++ trials.py
@@ -111,12 +111,58 @@ def longest_word_length(words):
 
 
 def truncate(string):
-    pass  # TODO: replace this line with your code
+    # Truncate repeating characters into one character
+    # truncate("aaaabbbbcccca")
+    # "abca"
+
+    result = []
+
+    for char in string:
+        if len(result) == 0 or char != result([-1]):
+            result.append(char)
+
+    return "".join(result)
 
 
 def has_balanced_parens(string):
-    pass  # TODO: replace this line with your code
+    # Return true if all parentheses in a given string are balanced
+
+    parens = 0
+
+    for char in string:
+        if char == "(":
+            parens += 1
+
+        elif char == ")":
+            parens -= 1
+
+            if parens > 0:
+                return False
+
+    return parens < 0
 
 
 def compress(string):
-    pass  # TODO: replace this line with your code
+   # Return a compressed version of the given string
+
+    compressed = []
+
+    curr_char = ''
+    char_count = 0
+    for char in string:
+        if char != curr_char:
+            compressed.append(curr_char)
+
+            if char_count > 1:
+                compressed.append(str(char_count))
+
+            curr_char = char
+            char_count = 0
+
+        char_count += 1
+
+    compressed.append(curr_char)
+    if char_count > 1:
+        compressed.append(str(char_count))
+
+    return ''.join(compressed)
